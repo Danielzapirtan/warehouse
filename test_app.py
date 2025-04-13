be4307ef3353492ec652c408d2efbf0934fada08
Danielzapirtan <137394552+Danielzapirtan@users.noreply.github.com>
2025-04-13 21:28:27 +0300

    Create test_app.py

---
+++ test_app.py
@@ -0,0 +1,10 @@
+import pytest
+from app import db, display_db_state, create_product
+
+def test_empty_database():
+    assert display_db_state() == "Database is empty."
+
+def test_create_product():
+    result = create_product("Test", "units")
+    assert "Test (units)" in result
+    assert len(db.products) == 1

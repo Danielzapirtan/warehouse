4355ba94bbc2036cf3fae7400c4a56f7474f3425
Danielzapirtan <137394552+Danielzapirtan@users.noreply.github.com>
2025-06-30 21:21:13 +0300

    Update init_db.sh

--- init_db.sh
+++ init_db.sh
@@ -1,15 +1,4 @@
 #! /bin/bash
 
-FOLDER=./WarehouseDB
-ALT_FOLDER=.
-FILENAME='db.json'
-
-if test -f $FOLDER/$FILENAME; then
-   true
-else
-    mkdir -p $FOLDER
-    cp -a $ALT_FOLDER/$FILENAME $FOLDER
-fi
-
 apt install python3-pip
 pip install -r requirements.txt

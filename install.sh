abde0b7e85a24cc2f210599997e58a4048573e2b
Danielzapirtan <antoniudanielzapirtan@gmail.com>
2025-07-11 08:53:47 +0000

    m

--- install.sh
+++ install.sh
@@ -6,5 +6,6 @@ rm -rf warehouse
 git clone https://github.com/Danielzapirtan/warehouse.git
 cd $HOME/warehouse
 pip install -r requirements.txt
+pkill -kill streamlit
 streamlit run app.py &>/dev/null &
 echo Visit http://localhost:8501

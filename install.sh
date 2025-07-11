2122147daa68936f5d21e0a9311ab87f5956b701
Danielzapirtan <137394552+Danielzapirtan@users.noreply.github.com>
2025-07-11 11:47:49 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,9 +2,9 @@
 
 cd $HOME
 sudo apt install git python3-pip
-pip install -r requirements.txt
 rm -rf warehouse
 git clone https://github.com/Danielzapirtan/warehouse.git
 cd warehouse
+pip install -r requirements.txt
 streamlit run app.py &>/dev/null &
 echo Visit http://localhost:8501

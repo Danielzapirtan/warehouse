632c55d399ccc6a08b8df933cf0d9103588f8767
Danielzapirtan <antoniudanielzapirtan@gmail.com>
2025-07-11 08:50:27 +0000

    m

--- install.sh
+++ install.sh
@@ -4,7 +4,7 @@ cd $HOME
 sudo apt install git python3-pip
 rm -rf warehouse
 git clone https://github.com/Danielzapirtan/warehouse.git
-cd warehouse
+cd $HOME/warehouse
 pip install -r requirements.txt
 streamlit run app.py &>/dev/null &
 echo Visit http://localhost:8501
